18fdaf907bd009eec4d9907c60fb6a98d2341208
suresh-reddy0810 <pernatisureshreddy2002@gmail.com>
2026-03-13 12:24:31 +0530

    Add files via upload

---
+++ environments/dev/variable.tf
@@ -0,0 +1,24 @@
+variable "rg_name" {}
+variable "location" {}
+
+variable "vnet_name" {}
+variable "vnet_cicdr" {}
+variable "sub1_name" {}
+variable "sub2_cicdr" {}
+variable "sub1_cicdr" {}
+variable "sub2_name" {}
+variable "nsg_name" {}
+
+
+variable "vm_name" {}
+variable "nic_name" {}
+variable "sub1_id" {}
+
+
+variable "storage_account_name" {}
+variable "sub2_private_id" {}
+
+variable "app_service_name" {}
+variable "service_plan_name" {}
+
+variable "aks_name" {}

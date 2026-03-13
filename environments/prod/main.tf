efda4c6d06b4b6d2bc379f2b18aaa93066c6ce0e
suresh-reddy0810 <pernatisureshreddy2002@gmail.com>
2026-03-13 13:00:02 +0530

    Update main.tf

--- environments/prod/main.tf
+++ environments/prod/main.tf
@@ -13,12 +13,15 @@ module "vnet" {
     sub2_name = var.sub2_name
     sub2_cicdr = var.sub2_cicdr
     nsg_name = var.nsg_name
+    rg_name = module.rg.rg_name
+    location = module.rg.location
 }
 
 module "vm" {
     source = "../../modules/vm/"
     vm_name = var.vm_name
     nic_name = var.nic_name
+
 
 }
 
@@ -36,4 +39,5 @@ module "webapp" {
 module "aks" {
     source = "../../modules/aks/"
     aks_name = var.aks_name
+
 }

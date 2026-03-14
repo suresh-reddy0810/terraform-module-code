31062ac1308f8d2a36e849d0403c196df0e526e9
suresh-reddy0810 <pernatisureshreddy2002@gmail.com>
2026-03-14 13:16:43 +0530

    Update main.tf

--- environments/prod/main.tf
+++ environments/prod/main.tf
@@ -32,7 +32,7 @@ module "storage" {
     storage_account_name = var.storage_account_name
     rg_name = module.rg.rg_name
     location = module.rg.location
-    sub2_private_id = module.vnet.sub2_private_id
+    sub2_id = module.vnet.sub2_id
 }
 
 module "webapp" {
